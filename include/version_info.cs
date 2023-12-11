d8e6bfb45b742da15d03e132a3c67e18aa044a1f
Daniel Danan <daniel.danan@microfocus.com>
2023-12-11 14:31:36 +0200

    new release to fix Description field  (change label + make it non mandatory)

--- include/version_info.cs
+++ include/version_info.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("OpenText")]
 [assembly: AssemblyCopyright("Copyright © OpenText 2023")]
 [assembly: AssemblyProduct("LoadRunner Enterprise Plugin")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
 [assembly: AssemblyTrademark("OpenText")]
